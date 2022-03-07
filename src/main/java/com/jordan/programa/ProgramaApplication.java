46560e3fdb2fae7f1bbb41b83007456da1bbc3a6
JordanTrindade <jordandimst@gmail.com>
2022-03-07 18:43:06 -0300

    criando operacao de instaciação

--- src/main/java/com/jordan/programa/ProgramaApplication.java
+++ src/main/java/com/jordan/programa/ProgramaApplication.java
@@ -1,16 +1,33 @@
 package com.jordan.programa;
 
+import java.util.Arrays;
 
+import com.jordan.programa.dominios.Categoria;
+import com.jordan.programa.repositories.CategoriaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProgramaApplication {
+public class ProgramaApplication implements CommandLineRunner {
 
+	@Autowired
+	private CategoriaRepository cR;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProgramaApplication.class, args);
-		
-		
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+
+		cR.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
